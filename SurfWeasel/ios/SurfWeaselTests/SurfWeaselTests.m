ff66e0b739ffc410cac369632adce1bf8c5b58df
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2022-05-08 14:21:56 -0400

    rebuilt react-native init

--- SurfWeasel/ios/SurfWeaselTests/SurfWeaselTests.m
+++ SurfWeasel/ios/SurfWeaselTests/SurfWeaselTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SurfWeaselTests : XCTestCase
+@interface surfWeaselTests : XCTestCase
 
 @end
 
-@implementation SurfWeaselTests
+@implementation surfWeaselTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
