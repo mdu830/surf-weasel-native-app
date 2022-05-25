58ecd9703648547644629b9feb8e05cd22412ee5
Mitch Duncan <mdu830@Mitchs-MacBook-Pro.local>
2022-05-24 21:20:38 -0400

    set up react navigation, global styles, and darkmode for stacks

--- SurfWeasel/App.js
+++ SurfWeasel/App.js
@@ -1,22 +1,27 @@
-import * as React from 'react';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import HomeScreen from './screens/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import ReportPage from './screens/ReportPage';
 
 const Stack = createNativeStackNavigator();
 
-export default App = () => {
+function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator
-      screenOptions={{
-        headerShown: false
-      }}>
-        <Stack.Screen name="Home" component={HomeScreen}/>
-        <Stack.Screen name="Results" component={ReportPage}/>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
--- SurfWeasel/screens/HomeScreen.js
+++ SurfWeasel/screens/HomeScreen.js
@@ -1,63 +1,40 @@
-/**
- * @format
- * @flow strict-local
- */
+import React from 'react';
 
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
- 
- import {
-   Colors,
-   Header,
- } from 'react-native/Libraries/NewAppScreen';
- 
- 
- export default HomeScreen = () => {
-     
-   const isDarkMode = useColorScheme() === 'dark';
- 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
- 
-   const styles = StyleSheet.create({
-     sectionContainer: {
-       marginTop: 32,
-       paddingHorizontal: 24,
-     },
-     sectionTitle: {
-       fontSize: 24,
-       fontWeight: '600',
-     },
-     sectionDescription: {
-       marginTop: 8,
-       fontSize: 18,
-       fontWeight: '400',
-     },
-     highlight: {
-       fontWeight: '700',
-     },
-   });
- 
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View style={{ backgroundColor: isDarkMode ? Colors.black : Colors.white }}>
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    useColorScheme,
+    View,
+} from 'react-native';
 
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
+import {
+    Colors,
+    Header
+} from 'react-native/Libraries/NewAppScreen';
+
+const styles = require('./style');
+
+const HomeScreen = () => {
+
+    const isDarkMode = useColorScheme() === 'dark';
+
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
+
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+            <ScrollView
+                contentInsetAdjustmentBehavior="automatic"
+                style={backgroundStyle}>
+                <Header />
+                <View style={{ backgroundColor: isDarkMode ? Colors.black : Colors.white }}>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+export default HomeScreen;
